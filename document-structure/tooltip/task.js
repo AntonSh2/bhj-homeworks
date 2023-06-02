f97893c6fbdc9b83ed238e2ffeba975f3a1835fe
Anton <guepard127@gmail.com>
2023-06-02 10:29:25 +0400

    Исправлено

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,47 +1,20 @@
 const toolTip = document.querySelectorAll('.has-tooltip');
-
-let activated = null;
+const toolEven = document.createElement('div');
 
 toolTip.forEach(element => {
     element.addEventListener('click', event => {
         event.preventDefault();
-        if(activated) {
-            activated.classList.remove('tooltip_active');
-            activated = null;
-        }
-        const toolEven = document.createElement('div');
         toolEven.className = 'tooltip';
-        toolEven.innerHTML = element.title;
-        const position = element.getBoundingClientRect();
-        const dataPosition = element.getAttribute('data-position')
-        switch (dataPosition) {
-            case 'top':
-                toolEven.style.left = `${position.left}px`;
-                toolEven.style.top = `${position.top - toolEven.offsetHeight}px`;
-                break;
-            case 'left':
-                toolEven.style.left = `${position.top - toolEven.offsetHeight}px`;
-                toolEven.style.top = `${position.left}px`;
-                break;
-            case 'right':
-                toolEven.style.left = `${position.right}px`;
-                toolEven.style.top = `${position.top}px`;
-                break;
-            case 'bottom':
-            default:
-                toolEven.style.left = `${position.left}px`;
-                toolEven.style.top = `${position.bottom}px`;
-                break;
-        }
-        document.body.appendChild(toolEven);
+        activated = event.target;
+        if(activated.title === toolEven.innerText) {
+            toolEven.classList.toggle('tooltip_active');
+            return;
+        };
+        toolEven.innerHTML = activated.title;
+        const {bottom, left} = activated.getBoundingClientRect();
+        toolEven.style = `left: ${left}px; top: ${bottom}px`;
+        activated.insertAdjacentElement('afterEnd', toolEven);
         toolEven.classList.add('tooltip_active');
-        activated = toolEven;
-    })
-})
 
-document.addEventListener('click', event => {
-    if (activated && !event.target.classList.contains('has-tooltip')) {
-        activated.classList.remove('tooltip_active');
-        activated = null;
-    }
+    });
 })
