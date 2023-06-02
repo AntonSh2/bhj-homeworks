efd1b9c84838686686f58ca563d782df98423aab
Anton <guepard127@gmail.com>
2023-06-02 09:41:13 +0400

    Исправлено

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,26 +1,24 @@
 let taskInput = document.getElementById("task__input");
 let tasksList = document.getElementById("tasks__list");
 
-const taskRemove = e => {
-	e.target.closest(".task").remove();
-}
-
-const addTask = e => {
-	tasksList.innerHTML +=
+const addTask = () => {
+	tasksList.insertAdjacentHTML (
+		 'afterEnd',
 		`<div class="task">
       <div class="task__title">
         ${taskInput.value}
       </div>
       <a href="#" class="task__remove">&times;</a>
-    </div>`;
+    </div>`);
 	taskInput.value = "";
-  [...(tasksList.getElementsByClassName("task__remove"))].forEach(element => {
-		element.addEventListener("click", taskRemove)
-	});
-	e.preventDefault();
+
+	const taskRemove = document.querySelector('.task__remove');
+
+    taskRemove.addEventListener('click', event => {
+    event.target.closest('.task').remove();
+   })
+
 }
 
 document.getElementById("tasks__add").addEventListener("click", addTask);
-taskInput.addEventListener("keypress", e => {
-	if (e.keyCode == 13) addTask(e);
-});
+ e.preventDefault();
